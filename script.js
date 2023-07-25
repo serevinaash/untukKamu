63a91347add778cde1ad08441fa6b4b8466548cf
serevinaash <140361909+serevinaash@users.noreply.github.com>
2023-07-25 22:19:11 +0700

    script.js

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ function clik (btn){
   button[1].style.display = "none"
   ctn.style.height = "250px"
   ctn.style.width = "270px"
-  return btn.textContent == "Iya 🤗" ? kata.textContent = "😍 ni buat si kamu 🍊" : kata.textContent = "😭😭😭😭😭😭"
+  return btn.textContent == "Iya 🤗" ? kata.textContent = " Dadah, Ati ati di jalan yaa,aku juga mau pergi ajah" : kata.textContent = "kenapa ga chat?"
   
 }
 function content(){
