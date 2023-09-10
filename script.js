daf461f4ca810c8e41b1a03e009bf95c313dd746
serevinasherlyy <140361909+serevinasherlyy@users.noreply.github.com>
2023-09-10 11:43:13 +0700

    script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const button = document.querySelectorAll(".content button")
 const ctn = document.querySelector(".content")
 
 function clik (btn){
-  btn.textContent == "Iya 🤗" ? img.src = "jempol.gif" : img.src = "jempol.gif" 
+  btn.textContent == "Iya 🤗" ? img.src = "jempol.gif" : img.src = "patahHati.gif" 
   button[0].style.display = "none"
   button[1].style.display = "none"
   ctn.style.height = "250px"
